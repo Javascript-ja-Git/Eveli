9200edab366c7f6c55233ad173f5c4b080476eff
evelikera <kera.eveli@gmail.com>
2025-10-21 14:55:19 +0300

    Update app.js

--- code/02/app.js
+++ code/02/app.js
@@ -66,7 +66,7 @@ if (parool === 'parool') {
 
 const existingParool = 'admin123';
 
-if (parool === existingParool) {
+if (parool !== existingParool) {
  alert('Tere tulemast!');
 } else {
     alert('Vale parool');
