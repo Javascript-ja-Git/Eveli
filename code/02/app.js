d2cc0c5c3c0edc96fd5a7b29789e2f80023f196d
evelikera <kera.eveli@gmail.com>
2025-10-21 12:10:14 +0300

    Vanuse arvutamine

--- code/02/app.js
+++ code/02/app.js
@@ -1,3 +1,13 @@
+// Tervituse programm
+/*
 const name = prompt ('Mis su nimi on?')
 
 alert('Tere ' + name); 
+*/
+
+// Vanuse arvutamine
+
+const year =  prompt ('Mis su sünniaasta on?')
+
+alert('2025' - year)
+
