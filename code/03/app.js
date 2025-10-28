096058cd89d7766e2f682c1a072047478621ac94
evelikera <kera.eveli@gmail.com>
2025-10-28 13:07:38 +0200

    Update app.js

---
+++ code/03/app.js
@@ -0,0 +1,34 @@
+/*let i = 0;
+//while - tsükkel
+while(i < 5){
+    console.log(i);
+    i++;
+}*/
+
+// Väljasta arvud 5 kuni 15
+
+/*
+let i = 5;
+
+while(i <= 15){
+    // console.log(i);
+    i++;
+}
+// for - tsükkel
+console.log(i)*/
+/*
+for(let i = 5; i <= 15; i++) {
+   // console.log(i)
+   }
+
+console.log(i) */
+
+for(let i = 0; i > 0; i++) {
+    console.log('hello');
+}
+
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i<=5)
